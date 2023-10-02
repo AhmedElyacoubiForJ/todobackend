f2e187dc5fdee503d12d198e121f2b1a723c7aa1
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-10-02 17:38:20 +0200

    project setup

---
+++ src/test/java/edu/yacoubi/todobackend/TodobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.yacoubi.todobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
