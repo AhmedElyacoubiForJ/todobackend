543cb74eb24e1ddfac8d6590c71955636fe30044
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-10-14 03:11:52 +0200

    added CommandLineRunner in main to implement some scenarios later

--- src/main/java/edu/yacoubi/todobackend/Main.java
+++ src/main/java/edu/yacoubi/todobackend/Main.java
@@ -1,7 +1,15 @@
 package edu.yacoubi.todobackend;
 
+import com.github.javafaker.Faker;
+import edu.yacoubi.todobackend.model.AppUser;
+import edu.yacoubi.todobackend.repository.AppUserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @SpringBootApplication
 public class Main {
@@ -10,4 +18,40 @@ public class Main {
 		SpringApplication.run(Main.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(
+			AppUserRepository appUserRepository) {
+
+		return args -> {
+			//
+			AppUser newAppUser = generateAppUser();
+			appUserRepository.save(newAppUser);
+
+			Optional<AppUser> userByEmail = appUserRepository.findUserByEmail(newAppUser.getEmail());
+
+			userByEmail.ifPresent(user -> {
+				System.out.println(user.getId());
+				System.out.println(user.getFirstName());
+				System.out.println(user.getLastName());
+				System.out.println(user.getEmail());
+			});
+
+		};
+	}
+
+	private static AppUser generateAppUser() {
+		Faker faker = new Faker();
+		String firstName = faker.name().firstName();
+		String lastName = faker.name().lastName();
+		String email = String.format("%s.%s@yacoubi.edu", firstName, lastName);
+
+		AppUser appUser = new AppUser(
+				firstName,
+				lastName,
+				email,
+				"asasas",
+				"wewe"
+		);
+		return appUser;
+	}
 }
--- src/test/java/edu/yacoubi/todobackend/repository/ToDoRepositoryTest.java
+++ src/test/java/edu/yacoubi/todobackend/repository/ToDoRepositoryTest.java
@@ -1,12 +1,4 @@
 package edu.yacoubi.todobackend.repository;
 
-import org.junit.jupiter.api.BeforeEach;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class ToDoRepositoryTest {
-
-    @BeforeEach
-    void setUp() {
-    }
-}
+class ToDoRepositoryTest { }
