8a856666593f3f27e3f83f4af070d64ad9c2ad51
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-10-16 02:30:30 +0200

    added exception to app user service

---
+++ src/main/java/edu/yacoubi/todobackend/EntityNotFoundException.java
@@ -0,0 +1,12 @@
+package edu.yacoubi.todobackend;
+
+public class EntityNotFoundException extends RuntimeException {
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
+    public EntityNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- src/main/java/edu/yacoubi/todobackend/service/AppUserServiceImpl.java
+++ src/main/java/edu/yacoubi/todobackend/service/AppUserServiceImpl.java
@@ -1,14 +1,17 @@
 package edu.yacoubi.todobackend.service;
 
+import edu.yacoubi.todobackend.EntityNotFoundException;
 import edu.yacoubi.todobackend.model.AppUser;
 import edu.yacoubi.todobackend.repository.AppUserRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class AppUserServiceImpl implements AppUserService {
 
     private final AppUserRepository appUserRepository;
@@ -25,7 +28,22 @@ public class AppUserServiceImpl implements AppUserService {
 
     @Override
     public AppUser findById(Long id) {
-        return appUserRepository.findById(id).get();
+        log.info("call findById...");
+
+        if (id == null) {
+            log.error("id is null");
+            throw new IllegalArgumentException();
+        }
+
+        return appUserRepository.findById(id)
+                .orElseThrow(() -> {
+                    String message = "user with ID :  " + id + " not found";
+                    EntityNotFoundException eNfE =
+                            new EntityNotFoundException(message);
+                    log.error("error in getting user with ID :  {}", id, eNfE);
+                    return eNfE;
+                }
+        );
     }
 
     @Override
@@ -34,8 +52,20 @@ public class AppUserServiceImpl implements AppUserService {
 
     @Override
     public AppUser findAppUserByEmail(String email) {
-        return appUserRepository
-                .findAppUserByEmail(email)
-                .get();
+        log.info("call findAppUserByEmail...");
+
+        if (email == null) {
+            log.error("email is null");
+            throw new IllegalArgumentException();
+        }
+        return appUserRepository.findAppUserByEmail(email)
+                .orElseThrow(() -> {
+                            String message = "user with EMAIL :  " + email + " not found";
+                            EntityNotFoundException eNfE =
+                                    new EntityNotFoundException(message);
+                            log.error("error in getting user with EMAIL :  {}", email, eNfE);
+                            return eNfE;
+                        }
+                );
     }
 }
